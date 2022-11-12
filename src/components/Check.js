fc9de950658ace5775401d3ecda83e98f56aca1b
Kanata-Nemoto <k.n.soccer.0513@gmail.com>
2022-11-12 17:43:19 +0900

    [create]コンポーネントファイル

---
+++ src/components/Check.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+class Check extends React.Component {
+  
+}
+
+export default Check;
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div className='header'>
+        <h1>都道府県別総人口推移グラフ</h1>
+      </div>
+    );
+  }
+}
+
+export default Header;
