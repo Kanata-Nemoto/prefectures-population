7008fb8dabccf96cef3bb407048c6a42339ea9dc
Kanata-Nemoto <k.n.soccer.0513@gmail.com>
2022-11-13 16:05:49 +0900

    [change]コンポーネントファイルの修正

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+class App extends React.Component {
+  constructor() {
+    
+  }
+
+  render() {
+    return (
+      
+    );
+  }
+}
+
+export default App;
